f4f355439e656d7cdb52f6f3a754e0408cbcbb60
slash <slash@slashware.net>
2018-09-08 12:03:18 -0500

    Collisions

--- src/main.js
+++ src/main.js
@@ -34,10 +34,13 @@ const p = [ // palette
 ]
 
 const Renderer = {
-  render(c,x,y,ins) {
+  render(c,m) {
+    x = m.x;
+    y = m.y;
+    ins = m.app;
     ins.split(":").forEach(i => {
       t = i.split(',');
-      c.fillStyle = p[parseInt(t[0])];
+      c.fillStyle = m.blink ? p[0] : p[parseInt(t[0])];
       this[t[1]](c,x,y,t.slice(2));
     });
   },
@@ -52,9 +55,12 @@ const Renderer = {
 // Mob classes
 
 class Mob {
-  constructor(app) {
+  constructor(app, lists) {
     this.app = a[app];
     this.dx = this.dy = this.ax = this.ay = 0;
+    lists.push(mobs);
+    lists.forEach(l => l.push(this));
+    this.lists = lists;
   }
   // update
   u(d) {
@@ -63,38 +69,14 @@ class Mob {
     this.x += this.dx * d;
     this.y += this.dy * d;
   }
-}
 
-class Ship extends Mob {
-  // Keyboard
-  k(){
-    var P = 300;
-    this.ay = 0;
-    this.ax = 0;
-    if (isDown(38)){ // Rise
-      this.ay = -P;
-    } else if (isDown(40)){ // Sink
-      this.ay = P;
-    } 
-    if (isDown(37)){
-      this.flipped = true;
-      this.ax = -P;
-    } else if (isDown(39)){
-      this.flipped = true;
-      this.ax = P;
-    }
-    if (isDown(32)) {
-      // TOOD: Throothle?
-      this.fire();
-    }
+  destroy() {
+    this.app = false;
+    this.lists.forEach(l => l.splice(l.indexOf(this), 1));
   }
-  fire() {
-    var b = new Mob('bullet');
-    b.x = this.x;
-    b.y = this.y;
-    b.dy = -rand.range(550, 600);
-    mobs.push(b);
-    layers[1].push(b);
+
+  collide(m) {
+    this.destroy();
   }
 }
 
@@ -120,9 +102,19 @@ function raf(fn) {
   });
 }
 
+
+// "Physics" (LOL)
+function collide(a, b) {
+  var dist = Math.abs(a.x-b.x) + Math.abs(a.y-b.y);
+  if (dist < a.size + b.size) {
+    a.collide(b);
+  }
+}
+
 // Game Loop
 var layers = [[],[],[]];
 var mobs = [];
+var enemies = [];
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 raf(function(d) {
@@ -131,11 +123,61 @@ raf(function(d) {
   layers.forEach(l => l.forEach(m => {
     m.k && m.k(); // TODO this is only for the ship, put it somewhere
     m.u(d);
-    Renderer.render(ctx,m.x,m.y,m.app);
+    m.app && Renderer.render(ctx,m);
+    m.hits && (m.hits === 'p' ? collide(player, m) : enemies.forEach(e => collide(e, m)));
   }))
 });
 
-// Game Setup
+// THE GAME!
+
+class Ship extends Mob {
+  k(){
+    var P = 300;
+    this.ay = 0;
+    this.ax = 0;
+    if (isDown(38)){ // Rise
+      this.ay = -P;
+    } else if (isDown(40)){ // Sink
+      this.ay = P;
+    } 
+    if (isDown(37)){
+      this.flipped = true;
+      this.ax = -P;
+    } else if (isDown(39)){
+      this.flipped = true;
+      this.ax = P;
+    }
+    if (isDown(32)) {
+      this.fire();
+    }
+  }
+  fire() {
+    var b = new Mob('bullet', [layers[1]]);
+    b.x = this.x;
+    b.y = this.y;
+    b.dy = -rand.range(550, 600);
+    b.size = 5;
+    b.hits = 'e'; // Enemy
+  }
+}
+
+class Enemy extends Mob {
+  constructor(hp, app, lists) {
+    super(app, lists);
+    this.hp = hp;
+  }
+
+  collide(m) {
+    this.hp--;
+    this.blink = true;
+    setTimeout(() => this.blink = false, 50);
+    if (this.hp <= 0) {
+      super.collide(m);
+    }
+    m.destroy();
+  }
+}
+
 
 const a = { // Appearances
   // c - Circle radius, palette index
@@ -143,31 +185,38 @@ const a = { // Appearances
   star1: "0,c,1",
   star2: "0,c,2",
   star3: "0,c,3",
-  bullet: "1,c,4" 
+  bullet: "1,c,4",
+  e1: "1,c,15"
 }
 
 for (var i = 0; i < 50; i++) {
-  var t = new Mob('star'+rand.range(1, 3), 0);
+  var t = new Mob('star'+rand.range(1, 3), [layers[0]]);
   t.x = rand.range(0, 800);
   t.y = rand.range(0, 600);
   t.dy = rand.range(50, 100);
-  mobs.push(t);
-  layers[0].push(t);
 }
 
-setInterval(() => {
+// Replenish stars
+setInterval(() => { // TODO: Replace this with something from the game loop to avoid issues when not rendering
   for (var i = 0; i < 50; i++) {
-    var t = new Mob('star'+rand.range(1, 3), 0);
+    var t = new Mob('star'+rand.range(1, 3), [layers[0]]);
     t.x = rand.range(0, 800);
     t.y = -rand.range(0, 600);
     t.dy = rand.range(50, 100);
-    mobs.push(t);
-    layers[0].push(t);
-  }  
+  }
 }, 3000);
 
-var s = new Ship('ship', 2);
-s.x = 400;
-s.y = 500;
-mobs.push(s);
-layers[2].push(s);
+var player = new Ship('ship', [layers[2]]);
+player.x = 400;
+player.y = 500;
+player.size = 20;
+
+// Enemies
+for (var i = 0; i < 5; i++) {
+  var e = new Enemy(200, 'e1', [enemies,layers[2]]);
+  e.x = 100 + i * 150;
+  e.y = -100;
+  e.dy = 50;
+  e.hits = 'p'; // Player
+  e.size = 15;
+}
